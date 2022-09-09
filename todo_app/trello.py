7b74998cf4fe72e30e5f11ee39fcaec577a0ad4f
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2022-09-09 22:24:11 +0100

    Adding and Testing Trello code from Cohort Call

--- todo_app/trello.py
+++ todo_app/trello.py
@@ -1,25 +1,3 @@
-from flask import Flask, render_template, request, redirect, url_for, requests, os
-from todo_app.data.session_items import get_items, add_item
-from todo_app.flask_config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config())
-
-@app.route('/')
-def index():
-    return render_template('index.html', list_items=get_items())
-
-@app.route('/additem', methods=['POST'])
-def additem():
-    add_item(request.form.get('title'))
-    return redirect(url_for('index'))
-    
-if __name__ == "__main__":
-    app.run(debug=True)
-    
-    
-    
- '''
 import requests
 import os
 
@@ -52,4 +30,3 @@ for trello_list in response_json:
 
 for c in list_of_cards:
     print(c)
- '''
