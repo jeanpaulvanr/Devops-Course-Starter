d916001aa5cf32196407b18a11ea85d407154035
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-04-03 21:11:02 +0100

    Module 3 Fixes

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,16 +1,9 @@
 import requests, os
 from todo_app.data.item import Item
 
-key = os.getenv("API_KEY")
-token = os.getenv("API_TOKEN")
-board_id = os.getenv("BOARD_ID")
-todo_id = os.getenv("TO_DO_ID")
-doing_id = os.getenv("DOING_ID")
-done_id = os.getenv("DONE_ID")
-
 def get_items_all():
 
-    response = requests.get(f"https://api.trello.com/1/boards/{board_id}/lists?key={key}&token={token}&cards=open")
+    response = requests.get(f"https://api.trello.com/1/boards/{board_id()}/lists?key={key()}&token={token()}&cards=open")
         
     response_json = response.json()
 
@@ -26,18 +19,36 @@ def get_items_all():
 
 def add_item(title):
 
-    item = requests.post(f"https://api.trello.com/1/cards?idList={todo_id}&key={key}&token={token}&name={title}")
+    item = requests.post(f"https://api.trello.com/1/cards?idList={todo_id()}&key={key()}&token={token()}&name={title}")
 
     return item
 
 def doing_item(card_id):
     
-    item = requests.put(f"https://api.trello.com/1/cards/{card_id}?idList={doing_id}&key={key}&token={token}&board_id={board_id}")
+    item = requests.put(f"https://api.trello.com/1/cards/{card_id}?idList={doing_id()}&key={key()}&token={token()}&board_id={board_id()}")
 
     return item
 
 def close_item(card_id):
 
-    item = requests.put(f"https://api.trello.com/1/cards/{card_id}?idList={done_id}&key={key}&token={token}&board_id={board_id}")
+    item = requests.put(f"https://api.trello.com/1/cards/{card_id}?idList={done_id()}&key={key()}&token={token()}&board_id={board_id()}")
     
     return item
+
+def key():
+    return os.getenv("API_KEY")
+
+def token():
+    return os.getenv("API_TOKEN")
+
+def board_id():
+    return os.getenv("BOARD_ID")
+
+def todo_id():
+    return os.getenv("TO_DO_ID")
+
+def doing_id():
+    return os.getenv("DOING_ID")
+
+def done_id():
+    return os.getenv("DONE_ID")
