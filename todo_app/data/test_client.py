219f4ac4b9d6f1aa5b89f1b8cae881b3f6e8a6a6
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-06-27 22:10:39 +0100

    Module 10 - Updated Test Code V1

--- todo_app/data/test_client.py
+++ todo_app/data/test_client.py
@@ -1,6 +1,7 @@
-import os, pytest, requests
+import  pytest
 from dotenv import load_dotenv, find_dotenv
 from todo_app import app
+import mongomock
 
 @pytest.fixture
 def client():
@@ -8,47 +9,7 @@ def client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
 
-    # Create the new app.
-    test_app = app.create_app()
-
-    # Use the app to create a test_client that can be used in our tests.
-    with test_app.test_client() as client:
-        yield client
-        
-def test_index_page(monkeypatch, client):
-    # This replaces any call to requests.get with our own function
-    monkeypatch.setattr(requests, 'get', stub)
-
-    response = client.get('/')
-
-    assert response.status_code == 200
-    assert 'Test card' in response.data.decode()
-class StubResponse():
-    def __init__(self, fake_response_data):
-        self.fake_response_data = fake_response_data
-
-    def json(self):
-        return self.fake_response_data
-
-def stub(url, params={}):
-
-    expected_url = f'https://api.trello.com/1/boards/{test_board_id()}/lists?key={test_key_id()}&token={test_token_id()}&cards=open'
-
-    if url == expected_url:
-        fake_response_data = [{
-            'id': '123abc',
-            'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card'}]
-        }]
-        return StubResponse(fake_response_data)
-
-    raise Exception(f'Integration test did not expect URL "{url}", instead expected "{expected_url}"')
-     
-def test_board_id():
-    return os.environ.get('BOARD_ID')
-
-def test_key_id():
-    return os.environ.get('API_KEY')
-
-def test_token_id():
-    return os.environ.get('API_TOKEN')
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
