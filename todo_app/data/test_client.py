140510c95f86b5269f64387e2bb7a6bac29ad63a
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-06-28 08:02:50 +0100

    Module 10 - Integration Test Still Not Working

--- todo_app/data/test_client.py
+++ todo_app/data/test_client.py
@@ -13,3 +13,33 @@ def client():
         test_app = app.create_app()
         with test_app.test_client() as client:
             yield client
+            
+def test_index_page(monkeypatch, client):
+    # This replaces any call to requests.get with our own function
+    monkeypatch.setattr(stub)
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert 'Test card' in response.data.decode()
+    
+class StubResponse():
+    def __init__(self, fake_response_data):
+        self.fake_response_data = fake_response_data
+
+    def json(self):
+        return self.fake_response_data
+
+def stub(url, params={}):
+
+    expected_url = f'fakemongo.com'
+
+    if url == expected_url:
+        fake_response_data = [{
+            '_id': '123abc',
+            'status': 'To Do',
+            'item name': 'Test card'
+        }]
+        return StubResponse(fake_response_data)
+
+    raise Exception(f'Integration test did not expect URL "{url}", instead expected "{expected_url}"')
