f41162d00ad9eabd1eefe71923e55098cf1cf3ff
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-07-25 22:23:40 +0100

    Part 1 - Step 4 - Unsure if working

--- main.tf
+++ main.tf
@@ -23,6 +23,70 @@ resource "azurerm_service_plan" "main" {
   sku_name            = "B1"
 }
 
+#Cosmos DB Account
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "acc" {
+  name                = "jp-todoapp-cosmos-db-${random_integer.ri.result}"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  capabilities {
+      name = "EnableServerless"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
+
+#Cosmos DB
+
+resource "azurerm_cosmosdb_mongo_database" "db" {
+  name                = "jp-todoapp-cosmos-mongo-db"
+  resource_group_name = data.azurerm_cosmosdb_account.acc.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.acc.name
+  throughput          = 400
+}
+
+#App
+
 resource "azurerm_linux_web_app" "main" {
   name                = "JP-Hello-World"
   location            = data.azurerm_resource_group.main.location
