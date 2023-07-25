2ea1aaf3024adf02d65a53640ed35bab9e774487
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-07-25 21:48:24 +0100

    Project 12 - Part 1 - Step 3 - Working

--- main.tf
+++ main.tf
@@ -14,3 +14,29 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
   name     = "Cohort26_JeaRys_ProjectExercise"
 }
+
+resource "azurerm_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name                = "JP-Hello-World"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    application_stack {
+      docker_image     = "appsvcsample/python-helloworld"
+      docker_image_tag = "latest"
+    }
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+  }
+}
