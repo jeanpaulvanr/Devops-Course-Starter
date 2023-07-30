ba151afc42558412fefab2e7c899aa5e47df2b6d
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-07-30 07:00:31 +0100

    Part 2 - Step 2 - Working

--- outputs.tf
+++ outputs.tf
@@ -6,4 +6,3 @@ output "cd_webhook" {
     value = "https://${azurerm_linux_web_app.main.site_credential[0].name}:${azurerm_linux_web_app.main.site_credential[0].password}@${azurerm_linux_web_app.main.name}.scm.azurewebsites.net/docker/hook"
     sensitive = true
 }
-
