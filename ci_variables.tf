bbd4bb9dfa49c17f85c689fa3437cc9aaf600719
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-07-31 23:29:21 +0100

    Part 4 - Step 3 - V4

--- ci_variables.tf
+++
@@ -1,6 +0,0 @@
-docker_url = "https://index.docker.io/v1"
-flask_app = "todo_app/app"
-flask_env = "development"
-website_diagnostics_enabled = "true"
-website_service_storage_enabled = "false"
-websites_port = 5000
