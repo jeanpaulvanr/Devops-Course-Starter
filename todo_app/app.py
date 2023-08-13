0cdcb6c9689c4d40579788dcf10ddb2b85a436a3
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-08-13 20:07:07 +0100

    Console and Loggly Working

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,13 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 from todo_app.data.todo_app_items import get_items_all, add_item, close_item
 from todo_app.flask_config import Config
 from todo_app.data.view_model import ViewModel
-from dotenv import load_dotenv
+from datetime import datetime
+from loggly.handlers import HTTPSHandler
+from logging import Formatter
 
 def create_app():
 
     app = Flask(__name__)
     app.config.from_object(Config())
-
+    app.logger.setLevel(app.config['LOG_LEVEL'])
+    if app.config['LOGGLY_TOKEN'] is not None:
+        handler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{app.config["LOGGLY_TOKEN"]}/tag/todo-app')
+    handler.setFormatter(
+        Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+    )
+    app.logger.addHandler(handler)
+    
     @app.route('/')
     def index():
         var_view_model = ViewModel(get_items_all())
@@ -16,12 +25,16 @@ def create_app():
 
     @app.route('/additem', methods=['POST'])
     def additem():
-        add_item(request.form.get('title'))
+        log_add_item = request.form.get('title')
+        add_item(log_add_item)
+        app.logger.info('The following Item has been added: %s', log_add_item)
         return redirect(url_for('index'))
 
     @app.route('/closeitem', methods=['POST'])
     def closeitem():
-        close_item(request.form.get('card_id'))
+        log_close_item = request.form.get('card_id')
+        close_item(log_close_item)
+        app.logger.info('The following Item has been closed: %s', log_close_item)
         return redirect(url_for('index'))
 
     if __name__ == "__main__":
