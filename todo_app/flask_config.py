272135cc5d856abf43a30fe3fe67682ca3c27ae4
Jean-Paul van Ryswyck <jpv@tfg-brands.co.uk>
2023-08-13 21:53:10 +0100

    Deploying Logging to Terraform

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -8,4 +8,4 @@ class Config:
         if not self.SECRET_KEY:
             raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
         self.LOG_LEVEL = os.environ.get('LOG_LEVEL')
-        self.LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
+        self.LOGGING_SERVICE_TOKEN = os.environ.get('LOGGING_SERVICE_TOKEN')
